b6756ddb4a0edd1403714bd761943cc6fd56c2f4
littlemissmiso <todayisdelightful@gmail.com>
2025-05-09 21:56:40 -0600

    Home page progress

--- scripts/home.js
+++ scripts/home.js
@@ -1,4 +1,10 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
 
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('active');
+    hamButton.classList.toggle('active');
+});
 
 
 
